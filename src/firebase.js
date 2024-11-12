4e8d8e549b69612401ab46602e80c55c0ed699b7
Case1490 <renzoramos414@gmail.com>
2024-11-12 10:56:56 -0500

    finalizado

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,19 @@
-// firebase.js
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // Si usarás Firestore
-import { getStorage } from "firebase/storage"; // Importa getStorage para Firebase Storage
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCVu3iVpruPKlSkpiMeSODyhjeBxXitgNI",
-  authDomain: "prinfer-72da9.firebaseapp.com",
-  projectId: "prinfer-72da9",
-  storageBucket: "prinfer-72da9.appspot.com",
-  messagingSenderId: "754363131781",
-  appId: "1:754363131781:web:30cee30e04900109b886ec",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-
-// Inicializar Firestore si lo necesitas
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-// Inicializar Firebase Storage
-const storage = getStorage(app); // Aquí inicializamos el Storage
-
-export { db, storage }; // Exportamos también el storage
+export { db, storage };
