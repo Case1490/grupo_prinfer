35a7448b0b9288ecde510b18ee58e431c575c0c4
Case1490 <renzoramos414@gmail.com>
2024-11-11 19:14:00 -0500

    9.5/10

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,72 @@
-import { useEffect } from 'react';
-import ContactImage from '../assets/contacto.jpg'
-import Map from './Map';
+import { useState, useEffect } from "react";
+import ContactImage from "../assets/contacto.jpg"; // Asegúrate de que la ruta sea correcta
+import Map from "./Map";
 
 const Contact = () => {
-  // Código para que nos lleve al inicio de la página
+  const [formData, setFormData] = useState({
+    nombres: "",
+    apellidos: "",
+    telefono: "",
+    email: "",
+    mensaje: "",
+  });
+
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   useEffect(() => {
-    window.scrollTo(0, 0); // Desplazarse hacia el inicio
+    window.scrollTo(0, 0);
   }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const validate = () => {
+    const errors = {};
+    if (!formData.nombres) errors.nombres = "Nombres es requerido";
+    if (!formData.apellidos) errors.apellidos = "Apellidos es requerido";
+    if (!formData.telefono) {
+      errors.telefono = "Teléfono es requerido";
+    } else if (!/^\d+$/.test(formData.telefono)) {
+      errors.telefono = "Teléfono debe contener solo números";
+    }
+    if (!formData.email) {
+      errors.email = "Email es requerido";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      errors.email = "Email es inválido";
+    }
+    if (!formData.mensaje) errors.mensaje = "Mensaje es requerido";
+    return errors;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const errors = validate();
+    setFormErrors(errors);
+
+    if (Object.keys(errors).length === 0) {
+      // Establecer el estado de "Enviando..." antes de realizar el envío
+      setIsSubmitting(true);
+
+      // Realizar un pequeño retraso antes de enviar el formulario
+      setTimeout(() => {
+        // Ahora se envía el formulario
+        e.target.submit(); // Enviar el formulario directamente
+      }, 500); // Ajusta el tiempo si es necesario
+    } else {
+      alert("Por favor, complete correctamente todos los campos");
+    }
+  };
 
   return (
     <>
-      <div className=" min-h-screen flex items-center justify-center pt-[220px]">
+      <div className="min-h-screen flex items-center justify-center pt-[220px]">
+        {/* Imagen */}
         <div className="">
           <img
             src={ContactImage}
@@ -19,44 +75,83 @@ const Contact = () => {
           />
         </div>
 
+        {/* Formulario */}
         <div className="w-[40%]">
           <div className="flex-1">
             <div className="max-w-md mx-auto px-8 py-6 bg-gray-100 rounded-lg shadow-lg">
               <h2 className="text-3xl font-semibold text-center text-gray-800 mb-4">
                 Contáctanos
               </h2>
-              <form>
+              <form
+                onSubmit={handleSubmit}
+                action="https://formsubmit.co/comercial1.prinfer@gmail.com"
+                method="POST"
+              >
                 <div className="mb-4">
-                  <label className="block text-gray-800 mb-1" htmlFor="name">
+                  <label className="block text-gray-800 mb-1" htmlFor="nombres">
                     Nombres
                   </label>
                   <input
-                    className="w-full px-4 py-2 bg-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-300 transition duration-300"
+                    className={`w-full px-4 py-2 bg-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-300 transition duration-300 ${
+                      formErrors.nombres ? "ring-2 ring-red-500" : ""
+                    }`}
                     placeholder="Escribe tus nombres"
                     type="text"
+                    name="nombres"
+                    value={formData.nombres}
+                    onChange={handleChange}
                   />
+                  {formErrors.nombres && (
+                    <p className="text-red-500 text-sm">{formErrors.nombres}</p>
+                  )}
                 </div>
 
                 <div className="mb-4">
-                  <label className="block text-gray-800 mb-1" htmlFor="name">
+                  <label
+                    className="block text-gray-800 mb-1"
+                    htmlFor="apellidos"
+                  >
                     Apellidos
                   </label>
                   <input
-                    className="w-full px-4 py-2 bg-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-300 transition duration-300"
+                    className={`w-full px-4 py-2 bg-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-300 transition duration-300 ${
+                      formErrors.apellidos ? "ring-2 ring-red-500" : ""
+                    }`}
                     placeholder="Escribe tus apellidos"
                     type="text"
+                    name="apellidos"
+                    value={formData.apellidos}
+                    onChange={handleChange}
                   />
+                  {formErrors.apellidos && (
+                    <p className="text-red-500 text-sm">
+                      {formErrors.apellidos}
+                    </p>
+                  )}
                 </div>
 
                 <div className="mb-4">
-                  <label className="block text-gray-800 mb-1" htmlFor="name">
+                  <label
+                    className="block text-gray-800 mb-1"
+                    htmlFor="telefono"
+                  >
                     Teléfono
                   </label>
                   <input
-                    className="w-full px-4 py-2 bg-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-300 transition duration-300"
+                    className={`w-full px-4 py-2 bg-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-300 transition duration-300 ${
+                      formErrors.telefono ? "ring-2 ring-red-500" : ""
+                    }`}
                     placeholder="Escribe número de teléfono"
                     type="text"
+                    name="telefono"
+                    value={formData.telefono}
+                    onChange={handleChange}
                   />
+                  {formErrors.telefono && (
+                    <p className="text-red-500 text-sm">
+                      {formErrors.telefono}
+                    </p>
+                  )}
                 </div>
 
                 <div className="mb-4">
@@ -64,40 +159,65 @@ const Contact = () => {
                     Email
                   </label>
                   <input
-                    className="w-full px-4 py-2 bg-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-300 transition duration-300"
+                    className={`w-full px-4 py-2 bg-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-300 transition duration-300 ${
+                      formErrors.email ? "ring-2 ring-red-500" : ""
+                    }`}
                     placeholder="Escribe tu email"
+                    type="email"
                     name="email"
                     id="email"
-                    type="email"
+                    value={formData.email}
+                    onChange={handleChange}
                   />
+                  {formErrors.email && (
+                    <p className="text-red-500 text-sm">{formErrors.email}</p>
+                  )}
                 </div>
+
                 <div className="mb-4">
-                  <label className="block text-gray-800 mb-1" htmlFor="message">
+                  <label className="block text-gray-800 mb-1" htmlFor="mensaje">
                     Mensaje
                   </label>
                   <textarea
-                    className="w-full px-4 py-2 bg-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-300 transition duration-300 resize-none"
+                    className={`w-full px-4 py-2 bg-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-300 transition duration-300 resize-none ${
+                      formErrors.mensaje ? "ring-2 ring-red-500" : ""
+                    }`}
                     rows="4"
                     placeholder="Escribe tu mensaje"
-                    name="message"
-                    id="message"
+                    name="mensaje"
+                    id="mensaje"
+                    value={formData.mensaje}
+                    onChange={handleChange}
                   ></textarea>
+                  {formErrors.mensaje && (
+                    <p className="text-red-500 text-sm">{formErrors.mensaje}</p>
+                  )}
                 </div>
+
                 <button
                   className="w-full bg-yellow-300 text-gray-800 py-2 px-4 rounded-lg hover:bg-yellow-400 transition duration-300 text-xl"
                   type="submit"
+                  disabled={isSubmitting}
                 >
-                  Enviar
+                  {isSubmitting ? "Enviando..." : "Enviar"}
                 </button>
+
+                <input
+                  type="hidden"
+                  name="_next"
+                  value="http://localhost:5173/contacto"
+                />
+                <input type="hidden" name="_captcha" value="false" />
               </form>
             </div>
           </div>
         </div>
       </div>
 
+      {/* Aquí está tu mapa o componente adicional */}
       <Map />
     </>
   );
-}
+};
 
-export default Contact
+export default Contact;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,69 +1,95 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebookF,
   faTwitter,
   faLinkedinIn,
 } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
+import MapIcon from "../icons/MapIcon";
+import EmailIcon from "../icons/EmailIcon";
+import Phone from "../icons/Phone";
 
 const Footer = () => {
+  const [email, setEmail] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    // Simular un envío con setTimeout (puedes reemplazarlo con tu lógica de backend)
+    setTimeout(() => {
+      alert("¡Te has suscrito exitosamente!");
+      setIsSubmitting(false);
+      setEmail(""); // Limpiar el campo de email después de la suscripción
+    }, 2000); // Simular 2 segundos de espera para el envío
+  };
+
   return (
     <footer className="bg-BlueDark text-white py-10">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8">
         <div className="space-y-4">
           <h4 className="text-lg font-semibold">Acerca de Nosotros</h4>
           <p className="text-sm">
-            Somos una empresa peruana dedica y especializada en la distribucion
-            de suministros industriales para los sectores de manufactura y
-            otros. Tomando como punto de partida el dar soporte al area de
-            logistica y compras, asistirlos y brindarles soluciones de manera
-            inmediata.
+            Somos una empresa peruana dedicada y especializada en la
+            distribución de suministros industriales para los sectores de
+            manufactura y otros. Tomando como punto de partida el dar soporte al
+            área de logística y compras, asistirlos y brindarles soluciones de
+            manera inmediata.
           </p>
         </div>
-        <div className="space-y-4">
+        <div className="space-y-4 flex flex-col items-center">
           <h4 className="text-lg font-semibold">Enlaces Rápidos</h4>
           <ul className="space-y-2 text-sm">
             <li>
-              <a href="/productos" className="hover:underline">
+              <Link to="/" className="hover:underline">
+                Inicio
+              </Link>
+            </li>
+            <li>
+              <Link to="/productos" className="hover:underline">
                 Productos
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="/servicios" className="hover:underline">
-                Servicios
-              </a>
+              <Link to="/novedades" className="hover:underline">
+                Novedades
+              </Link>
             </li>
             <li>
-              <a href="/soporte-tecnico" className="hover:underline">
-                Soporte Técnico
-              </a>
+              <Link to="/nosotros" className="hover:underline">
+                Sobre Nosotros
+              </Link>
             </li>
             <li>
-              <a href="/catalogo" className="hover:underline">
-                Catálogo
-              </a>
-            </li>
-            <li>
-              <a href="/contacto" className="hover:underline">
+              <Link to="/contacto" className="hover:underline">
                 Contacto
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
         <div className="space-y-4">
           <h4 className="text-lg font-semibold">Contacto</h4>
-          <p className="text-md">
-            123 Calle Industrial,
-            <br />
+          <p className="text-md flex">
+            <MapIcon />
+            123 Calle Industrial, <br />
             Ciudad, País
           </p>
-          <p className="text-md">+51 917 279 856</p>
-          <p className="text-md">
-            <a
-              href="mailto:comercial1.prinfer@gmail.com"
-              className="hover:underline"
-            >
-              comercial1.prinfer@gmail.com
-            </a>
+          <div className="space-y-2">
+            <p className="text-md flex items-center space-x-2">
+              <Phone /> +51 917 279 856
+            </p>
+            <p className="text-md flex items-center space-x-2">
+              <Phone /> +51 902 089 114
+            </p>
+            <p className="text-md flex items-center space-x-2">
+              <Phone /> +51 908 918 768
+            </p>
+          </div>
+
+          <p className="text-md flex items-center gap-x-2">
+            <EmailIcon /> comercial1.prinfer@gmail.com
           </p>
         </div>
         <div className="space-y-4">
@@ -71,15 +97,35 @@ const Footer = () => {
           <p className="text-sm">
             Recibe nuestras últimas noticias y ofertas especiales.
           </p>
-          <form className="flex flex-col space-y-2">
+          <form
+            className="flex flex-col space-y-2"
+            onSubmit={handleSubmit}
+            action="https://formsubmit.co/comercial1.prinfer@gmail.com"
+            method="POST"
+          >
             <input
               type="email"
+              value={email}
+              name="email"
+              onChange={(e) => setEmail(e.target.value)}
               placeholder="Tu correo electrónico"
               className="p-2 rounded-md text-black outline-none"
+              required
             />
-            <button className="bg-YellowMain uppercase font-bold hover:bg-yellow-500 text-BlueDark py-2 px-4 rounded-md">
-              Suscribirse
+            <button
+              type="submit"
+              className="bg-YellowMain uppercase font-bold hover:bg-yellow-500 text-BlueDark py-2 px-4 rounded-md"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? "Enviando..." : "Suscribirse"}
             </button>
+
+            <input
+              type="hidden"
+              name="_next"
+              value="http://localhost:5173/contacto"
+            />
+            <input type="hidden" name="_captcha" value="false" />
           </form>
         </div>
       </div>
